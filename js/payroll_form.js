81c7d7639de93eebeaab6a0a4ffe8cf53aa41009
ChandradipMoonlight <chandradipshivankar@gmail.com>
2021-06-30 23:10:49 +0530

    Added: js code to set even listener on salary

--- js/payroll_form.js
+++ js/payroll_form.js
@@ -3,4 +3,5 @@ const output = document.querySelector('.salary-output');
 output.textContent = salary.value;
 salary.addEventListener('input', function() {
     output.textContent = salary.value;
+
 });
