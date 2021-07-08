5222ae0879281030a226f8c6ebb83380a96637cb
ChandradipMoonlight <chandradipshivankar@gmail.com>
2021-07-08 19:12:59 +0530

    Added: template literals to view data in tabular formate

--- js/home.js
+++ js/home.js
@@ -8,19 +8,18 @@ const createInnerHtml = () => {
         "<th>Salary</th><th>Start Date</th><th>Action</th>";
     const innerHtml = `${headerHtml}
     <tr>
-        <td><img class="profile" alt="" src="\assets\profile-images\Ellipse -2.png">
+    <td>
+        <img class="profile" src="\assets\profile-images\Ellipse -9.png">
     </td>
     <td>Chandradip Shivankar</td>
     <td>Male</td>
     <td><div class="dept-label">HR</div>
-            <div class="dept-label">Finance</div></td>
+    <div class="dept-label">Finance</div></td>
     <td>450000</td>
     <td>1 Nov 2020</td>
     <td>
-        <img id="1" onclick="remove(this)" alt="delete"
-            src="/assets/icons/delete-black-18dp.svg">
-        <img id="1" alt="edit" onclick="update(this)" 
-            src="/assets/icons/create-black-18dp.svg">
+        <img id="1" onclick="remove(this)" alt="delete" src="/assets/icons/delete-black-18dp.svg">
+        <img id="1" alt="edit" onclick="update(this)" src="/assets/icons/create-black-18dp.svg">
     </td>
     </tr>
 `;
